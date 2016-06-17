327e562971c65eaede89d7aea858bbf325208ac0
wangzhengyi <wzyll1314@gmail.com>
2016-06-17 20:01:17 +0800

    提交步数界面代码

---
+++ app/src/main/java/com/watch/qqhealthy/view/Bezier.java
@@ -0,0 +1,94 @@
+package com.watch.qqhealthy.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class Bezier extends View {
+    private Paint mPaint;
+    private int mCenterX, mCenterY;
+    private PointF start, end, control;
+
+
+    public Bezier(Context context) {
+        this(context, null);
+    }
+
+    public Bezier(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public Bezier(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStrokeWidth(8);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setTextSize(60);
+
+        start = new PointF(0, 0);
+        end = new PointF(0, 0);
+        control = new PointF(0, 0);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mCenterX = w / 2;
+        mCenterY = h / 2;
+
+        // 初始化数据点和控制点位置
+        start.x = mCenterX - 200;
+        start.y = mCenterY;
+
+        end.x = mCenterX + 200;
+        end.y = mCenterY;
+
+        control.x = mCenterX;
+        control.y = mCenterY - 100;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // 绘制数据点和控制点
+        mPaint.setColor(Color.GRAY);
+        mPaint.setStrokeWidth(20);
+        canvas.drawPoint(start.x, start.y, mPaint);
+        canvas.drawPoint(end.x, end.y, mPaint);
+        canvas.drawPoint(control.x, control.y, mPaint);
+
+        // 绘制辅助线
+        mPaint.setStrokeWidth(4);
+        canvas.drawLine(start.x, start.y, control.x, control.y, mPaint);
+        canvas.drawLine(control.x, control.y, end.x, end.y, mPaint);
+
+        // 绘制贝塞尔曲线
+        mPaint.setColor(Color.RED);
+        mPaint.setStrokeWidth(8);
+
+        Path path = new Path();
+
+        path.moveTo(start.x, start.y);
+        path.quadTo(control.x, control.y, end.x, end.y);
+
+        canvas.drawPath(path, mPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        control.x = event.getX();
+        control.y = event.getY();
+        invalidate();
+        return super.onTouchEvent(event);
+    }
+}
---
+++ app/src/main/java/com/watch/qqhealthy/view/QQHealthyView.java
@@ -0,0 +1,212 @@
+package com.watch.qqhealthy.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.View;
+
+import com.watch.qqhealthy.R;
+
+public class QQHealthyView extends View {
+    private static final int BG_START_ANGLE = 120;
+    private static final int BG_SWEEP_ANGLE = 300;
+    private static final int MARGIN = 20;
+    private int mTextColor = Color.BLACK;
+    private int mLineColor = Color.BLACK;
+
+    /**
+     * View的宽度
+     */
+    private int mWidth;
+
+    /**
+     * View的高度
+     */
+    private int mHeight;
+
+    /**
+     * 外层圆弧矩形的画笔
+     */
+    private Paint mBgPaint;
+
+    /**
+     * 外层圆弧矩形
+     */
+    private Path mBgPath;
+
+    /**
+     * 进度背景画笔
+     */
+    private Paint mArcBgPaint;
+
+    /**
+     * 进度矩形
+     */
+    private RectF mArcRectF;
+
+    /**
+     * 进度画笔
+     */
+    private Paint mArcProgressPaint;
+
+    /**
+     * 进度
+     */
+    private float mProgress = 30;
+
+    private Paint mWalkNumPaint;
+
+    /**
+     * 走路步数
+     */
+    private int mWalkNum = 1000;
+
+    public QQHealthyView(Context context) {
+        this(context, null);
+    }
+
+    public QQHealthyView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public QQHealthyView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        TypedArray ta = context.getTheme().obtainStyledAttributes(attrs, R.styleable.QQHealthyView,
+                defStyleAttr, 0);
+
+        mTextColor = ta.getColor(R.styleable.QQHealthyView_titleColor, mTextColor);
+        mLineColor = ta.getColor(R.styleable.QQHealthyView_lineColor, mLineColor);
+
+        ta.recycle();
+
+        init();
+    }
+
+    private void init() {
+        mBgPath = new Path();
+        initPaint();
+    }
+
+    private void initPaint() {
+        mBgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mBgPaint.setStrokeWidth(10);
+        mBgPaint.setStyle(Paint.Style.STROKE);
+        mBgPaint.setColor(Color.BLUE);
+
+        mArcBgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mArcBgPaint.setStyle(Paint.Style.STROKE);
+        mArcBgPaint.setDither(true);
+        mArcBgPaint.setStrokeJoin(Paint.Join.ROUND);
+        mArcBgPaint.setStrokeCap(Paint.Cap.ROUND);
+        mArcBgPaint.setColor(mLineColor);
+        mArcBgPaint.setStrokeWidth(50);
+
+        mArcProgressPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mArcProgressPaint.setStyle(Paint.Style.STROKE);
+        mArcProgressPaint.setDither(true);
+        mArcProgressPaint.setStrokeJoin(Paint.Join.ROUND);
+        mArcProgressPaint.setStrokeCap(Paint.Cap.ROUND);
+        mArcProgressPaint.setColor(mTextColor);
+        mArcProgressPaint.setStrokeWidth(50);
+
+        mWalkNumPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mWalkNumPaint.setTextSize(120);
+        mWalkNumPaint.setColor(mTextColor);
+        mWalkNumPaint.setTextAlign(Paint.Align.CENTER);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+
+        if (widthMode == MeasureSpec.EXACTLY) {
+            mWidth = widthSize;
+        } else {
+            mWidth = (int) (metrics.widthPixels * 1.0 / 2);
+            if (widthMode == MeasureSpec.AT_MOST) {
+                mWidth = Math.min(mWidth, widthSize);
+            }
+        }
+
+        if (heightMode == MeasureSpec.EXACTLY) {
+            mHeight = heightSize;
+        } else {
+            mHeight = (int) (metrics.heightPixels * 3.0 / 4);
+            if (heightMode == MeasureSpec.AT_MOST) {
+                mHeight = Math.min(mHeight, heightSize);
+            }
+        }
+
+        setMeasuredDimension(mWidth, mHeight);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+        mHeight = h;
+
+        int len = Math.min(mWidth, mHeight);
+        float left = (float) (len * 1.0 / 4);
+        float top = (float) (len * 1.0 / 4);
+        float right = (float) (len * 3.0 / 4);
+        float bottom = (float) (len * 3.0 / 4);
+
+        mArcRectF = new RectF(left, top, right, bottom);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        drawBackground(canvas);
+        drawArcAndText(canvas);
+    }
+
+    private void drawArcAndText(Canvas canvas) {
+        // 绘制背景圆弧
+        canvas.drawArc(mArcRectF, BG_START_ANGLE, BG_SWEEP_ANGLE, false, mArcBgPaint);
+
+        // 绘制进度圆弧
+        canvas.drawArc(mArcRectF, BG_START_ANGLE, mProgress, false, mArcProgressPaint);
+
+        // 绘制圈内数字
+        Paint.FontMetricsInt fm = mWalkNumPaint.getFontMetricsInt();
+        float baselineY = mWidth / 2 - (fm.descent + fm.ascent) / 2;
+        canvas.drawText(String.valueOf(mWalkNum), mWidth / 2, baselineY, mWalkNumPaint);
+    }
+
+    private void drawBackground(Canvas canvas) {
+        int radiusBg = mWidth / 20;
+        // 绘制左边的直线
+        mBgPath.moveTo(MARGIN, mHeight - MARGIN);
+        mBgPath.lineTo(MARGIN, radiusBg);
+
+        // 绘制左边圆角
+        mBgPath.quadTo(MARGIN, MARGIN, radiusBg + MARGIN, MARGIN);
+
+        // 绘制上方直线
+        mBgPath.lineTo(mWidth - radiusBg - MARGIN, MARGIN);
+
+        // 绘制右边圆角
+        mBgPath.quadTo(mWidth - MARGIN, MARGIN, mWidth - MARGIN, radiusBg + MARGIN);
+
+        // 绘制右边直线
+        mBgPath.lineTo(mWidth - MARGIN, mHeight - MARGIN);
+
+        // 绘制下方直线
+        mBgPath.close();
+
+        canvas.drawPath(mBgPath, mBgPaint);
+    }
+}
